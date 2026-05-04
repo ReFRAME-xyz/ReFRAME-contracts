0d81b5f737c64d242f52701f2e7307cc93319772
Bitzen01 <shayan.irdev@gmail.com>
2026-05-04 18:21:31 +0330

    Access control

--- contracts/ERC1155SingleTokenFrame.sol
+++ contracts/ERC1155SingleTokenFrame.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
-
 import "./interfaces/IFrame.sol";
 
-contract ERC1155SingleTokenFrame is ERC1155Burnable, IFrame {
+contract ERC1155SingleTokenFrame is ERC1155Burnable, IFrame, AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     uint256 public constant MAX_SUPPLY = 1;
     uint256 public constant TOKEN_ID = 1;
 
@@ -24,6 +27,10 @@ contract ERC1155SingleTokenFrame is ERC1155Burnable, IFrame {
         string memory symbol_,
         uint256 _royaltyPercentage
     ) ERC1155("") IFrame() {
+        _grantRole(DEFAULT_ADMIN_ROLE, tx.origin);
+        _grantRole(ADMIN_ROLE, tx.origin);
+        _grantRole(MINTER_ROLE, tx.origin);
+
         if (
             _royaltyPercentage != 0 &&
             (_royaltyPercentage < 5 || _royaltyPercentage > 100)
@@ -56,5 +63,11 @@ contract ERC1155SingleTokenFrame is ERC1155Burnable, IFrame {
 
     function symbol() public view virtual returns (string memory) {
         return _symbol;
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
--- contracts/ERC721SingleTokenFrame.sol
+++ contracts/ERC721SingleTokenFrame.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./interfaces/IFrame.sol";
 
-contract ERC721SingleTokenFrame is ERC721Burnable, IFrame {
+contract ERC721SingleTokenFrame is ERC721Burnable, IFrame, AccessControl {
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     uint256 public constant MAX_SUPPLY = 1;
     uint256 public royaltyPercentage;
     address public creator;
@@ -15,6 +18,10 @@ contract ERC721SingleTokenFrame is ERC721Burnable, IFrame {
         string memory _symbol,
         uint256 _royaltyPercentage
     ) ERC721(_name, _symbol) IFrame() {
+        _grantRole(DEFAULT_ADMIN_ROLE, tx.origin);
+        _grantRole(ADMIN_ROLE, tx.origin);
+        _grantRole(MINTER_ROLE, tx.origin);
+
         if (
             _royaltyPercentage != 0 &&
             (_royaltyPercentage < 5 || _royaltyPercentage > 100)
@@ -24,6 +31,12 @@ contract ERC721SingleTokenFrame is ERC721Burnable, IFrame {
         royaltyPercentage = _royaltyPercentage;
         creator = tx.origin;
         _mint(tx.origin, 1);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) public view virtual override(ERC721, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     function _baseURI() internal pure override returns (string memory) {
