37cea08c0ca6f7cea2f44db29a6874d967496251
Bitzen01 <shayan.irdev@gmail.com>
2026-06-08 20:08:08 +0330

    deployments

--- ignition/modules/ERC1155SingleTokenFactory.ts
+++ ignition/modules/ERC1155SingleTokenFactory.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("ERC1155SingleTokenFactoryV1", (m) => {
-  const factory = m.contract("ERC1155SingleTokenFactory");
+export default buildModule("ERC1155SingleTokenFactoryV2", (m) => {
+  const factory = m.contract("ERC1155SingleTokenFactory", [
+    "https://nft.reframeit.xyz/api/v1/metadata/"
+  ]);
 
   return { factory };
 });
--- ignition/modules/ERC721SingleTokenFactory.ts
+++ ignition/modules/ERC721SingleTokenFactory.ts
@@ -1,7 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-export default buildModule("ERC721SingleTokenFactoryV1", (m) => {
-  const factory = m.contract("ERC721SingleTokenFactory");
+export default buildModule("ERC721SingleTokenFactoryV2", (m) => {
+  const factory = m.contract("ERC721SingleTokenFactory", [
+    "https://nft.reframeit.xyz/api/v1/metadata/"
+  ]);
 
   return { factory };
 });
